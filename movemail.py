7fa62287109f52d636dbcc2e488f6b0afd7b18e9
Anish Shankar <rndanish@gmail.com>
2013-03-01 19:01:50 +0530

    Added folder/label specification support fixed a tiny message displaying mixup while connecting

--- movemail.py
+++ movemail.py
@@ -9,6 +9,23 @@ def default_hosts(s):
         return "students.iiit.ac.in"
     return s
 
+def select_folder(con):
+    v = map(lambda l: l.split()[2].decode() ,con.list()[1])
+    s = ""
+    while True:
+        for i in xrange(len(v)):
+            print "%d %s" %(i+1,v[i])
+        s = raw_input()
+        if len(s) != 0 and ((not s.isdigit()) or int(s) <= 0 or int(s) > len(v)):
+            print "Error Invalid Selection"
+        else:
+            break
+
+    if len(s) == 0 :
+        con.select()        
+    else:
+        con.select(mailbox=v[int(s)-1].strip('"'))
+
 def get_input():
     global src_host,dest_host
     global scon,dcon
@@ -24,25 +41,27 @@ def get_input():
     duser=raw_input()
     dpass=getpass.getpass()
 
-    print "Connecting to %s"%dest_host
+    print "Connecting to %s"%src_host
     scon=imaplib.IMAP4_SSL(src_host)
     print "Connected!"
     scon.login(suser,spass)
     print "Authenticated!"
-    scon.select()
+    print "Select Source Folder to copy from (default INBOX):"
+    select_folder(scon)
 
-    print "Connecting to %s"%src_host
+    print "Connecting to %s"%dest_host
     dcon=imaplib.IMAP4_SSL(dest_host)
     print "Connected!"
     dcon.login(duser,dpass)
     print "Authenticated!"
-    dcon.select()
+    print "Select Destination Folder to copy to (default INBOX):"
+    select_folder(dcon)
 
 def move_messages():
-    print "Getting List of Messages in Inbox"
+    print "Getting List of Messages in Folder"
     message_list=scon.search(None,'ALL')[1][0].split()
     print "%d Messages in INBOX to be moved"%len(message_list)
-    print "Getting Details of Messages in Inbox"
+    print "Getting Details of Messages in Folder"
     dat=scon.fetch(','.join(message_list),"(BODY[HEADER.FIELDS (subject)])")[1]
     subjects={}
     for i in dat:
